ec823509f541483b77c51bee6468bc2b3f5a1165
kzsnkgw <kazushi.nakagaw@jp.oro.com>
2016-02-05 11:56:45 +0900

    Revert "Initial"
    This reverts commit 6d6a950e6c5f369f5bb1f7b0a2f8652f6d4afcae.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var csscomb = require('gulp-csscomb');
-
-gulp.task('styles', function(){
-    return gulp.src('./src/**/*.*')
-        .pipe(csscomb())
-        .pipe(gulp.dest('./dist/'));
-});
